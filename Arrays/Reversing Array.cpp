7717e00d4ee5fe993033f9e9f65eacc6200f9b7b
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-06-19 19:23:44 -0700

    Update Reversing Array.cpp

--- Arrays/Reversing Array.cpp
+++ Arrays/Reversing Array.cpp
@@ -9,16 +9,16 @@ using namespace std;
 
 int main()
 {
-    int a[9] = {1,2,3,4,5,6,9,10,12};
-    int b[9];
-    int j = 0;
+    int a[9] = {1,2,3,4,5,6,9,10,12}; // Initializing Array
+    int b[9]; // Declaring Array to store the reverse Array
+    int j = 0; // Variable that will reperesent the indicies of B[j]
 
-    for (int i = 8; i >= 0; i--){
+    for (int i = 8; i >= 0; i--){ // Loop to reverse
         b[j] = a[i];
         j = j + 1;
     }
     
-    for (int i = 0; i < 9; i++){
+    for (int i = 0; i < 9; i++){ // Loop to print the reversed Array
         cout << b[i]  << " ";
     }
 
