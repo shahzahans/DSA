7d707e9833445f570c94a2c4b9e9435f91a26e90
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-06-19 19:21:16 -0700

    Update Counting Even #'s.cpp

--- Arrays/Counting Even #'s.cpp
+++ Arrays/Counting Even #'s.cpp
@@ -9,16 +9,16 @@ using namespace std;
 
 int main()
 {
-    int a[] = {1,2,3,4,5,6,9,10,12};
-    int count = 0;
+    int a[] = {1,2,3,4,5,6,9,10,12}; // Initializing Array
+    int count = 0; // Creating and initializing a varible to count the even numbers included in Array
 
-    for (int i = 0; i < 9; i++){
+    for (int i = 0; i < 9; i++){ // Loop to traverse the Array to find the even numbers
         if(a[i] % 2 == 0){
             count++;
         }
     }
     
-    cout << "There are " << count << " even numbers." << endl;
+    cout << "There are " << count << " even numbers." << endl; // Printing the counted Even numbers within the Array
 
     return 0;
 }
