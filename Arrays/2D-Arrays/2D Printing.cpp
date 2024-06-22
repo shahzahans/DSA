b0dfc33cc1a075c58c5baa7faf133de9e64bd76f
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-06-21 21:22:09 -0700

    Update 2D Printing.cpp

--- Arrays/2D-Arrays/2D Printing.cpp
+++ Arrays/2D-Arrays/2D Printing.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 int main()
 {
-    int a [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int a [3][3] = {{1,2,3},{4,5,6},{7,8,9}}; // Initialize the Array
 
-    for (int i=0; i<3; i++){
-        for (int j=0; j<3; j++){
-            cout << a[i][j] << " ";
+    for (int i=0; i<3; i++){ // Loop to traverse through the row
+        for (int j=0; j<3; j++){ // Loop to traverse through the column
+            cout << a[i][j] << " "; // Prints the 2D Arrays
         }
     cout << endl;
 }
