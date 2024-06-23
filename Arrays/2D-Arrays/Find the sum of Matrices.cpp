8918f3f7e93305a18b336d8f2e32ecda93d08c42
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-06-22 20:10:49 -0700

    Update Find the sum of Matrices.cpp

--- Arrays/2D-Arrays/Find the sum of Matrices.cpp
+++ Arrays/2D-Arrays/Find the sum of Matrices.cpp
@@ -1,7 +1,7 @@
 /**
  * Practice Problem ~ Shadman S.
  * Topic: 2D Array'S
- * Problem Statement: Given a matrices (2D~Array) the task is to add them
+ * Problem Statement: Given a matrix (2D~Array) the task is to add them
  */
 
 #include <iostream>
@@ -9,18 +9,18 @@ using namespace std;
 
 int main()
 {
-    int a [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int b [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int a [3][3] = {{1,2,3},{4,5,6},{7,8,9}}; // Initializing fisrt set of array
+    int b [3][3] = {{1,2,3},{4,5,6},{7,8,9}}; // Initializing second set of array
     
-    int sum[3][3];
+    int sum[3][3]; // intilizing variable with 2D Array
 
-    for (int i=0; i<3; i++){
+    for (int i=0; i<3; i++){ // For loop add both 2d arrays
         for (int j=0; j<3; j++){
             sum [i][j] = a[i][j] + b[i][j];
         }
 }
 
-    for (int i=0; i<3; i++){
+    for (int i=0; i<3; i++){ // Loop to print the added Array
         for (int j=0; j<3; j++){
             cout << sum [i][j] <<  " ";
         }
