c558df9b6b5a71fbefb2c801b89210369388a69f
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-06-23 21:21:47 -0700

    Update Finding Positioning of an 2D Array.cpp

--- Arrays/2D-Arrays/Finding Positioning of an 2D Array.cpp
+++ Arrays/2D-Arrays/Finding Positioning of an 2D Array.cpp
@@ -11,24 +11,24 @@ using namespace std;
 
 int main()
 {
-    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}}; // Initializing 2D ARRAY
     
-    int x;
-    int flag = 0;
+    int x; // Initializing variable 
+    int flag = 0; // created flag variable to confirm if-else-cases
     
     cout << "Enter the number to find its position: ";
     cin >> x;
     
     
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++){ // Loop through 2d Array to find the position of the element
         for (int j = 0; j < 3; j++){
             if (a[i][j] == x){
-                flag = 1;
+                flag = 1; // Confirm If statement
                 cout << "The number " << x << " position in the element is [" << i << "]["<< j << "]" << endl;
             }
         }
     }
-    if (flag == 0){
+    if (flag == 0){ // Confirms if the element position is not found within the Array
         cout << "Element " << x << " is not present in the Array" << endl;
     }
     
