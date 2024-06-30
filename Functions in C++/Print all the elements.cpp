1f0266dd6f37a31b942e39622bdaade38a8911f4
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-06-29 23:32:00 -0700

    Update Print all the elements.cpp

--- Functions in C++/Print all the elements.cpp
+++ Functions in C++/Print all the elements.cpp
@@ -7,25 +7,25 @@
 using namespace std;
 
 
-void printArray(int a[], int n){
-    for (int i = 0; i < n ; i++){
-        if (a[i] == n ){
-            cout << a[i];
+void printArray(int a[], int n){ // Function to print the all the elements
+    for (int i = 0; i < n ; i++){ loop to iterate through whole array
+        if (a[i] == n ){ // if-statement, to print the last element without the comma
+            cout << a[i]; 
         }
-        else{
+        else{ // to print every other element through a comma
             cout << a[i] << ",";
         }
     }
 }
 
 int main(){
-    int a[] = {1,2,3,4,5,6,7,8,9,10};
+    int a[] = {1,2,3,4,5,6,7,8,9,10}; // initialized the array
     
-    int n = sizeof(a)/sizeof(a[0]);
+    int n = sizeof(a)/sizeof(a[0]); // finding the size of the element
     
     cout << "The size of Array is " << n << " and the element that is present within the array is {";
     
-    printArray (a,n);
+    printArray (a,n); // calling the function using set of arguements
     
     cout << "}";
     
