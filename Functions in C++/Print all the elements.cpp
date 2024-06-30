2edc752e54ff53140d4cec8e6e5522793878f0bd
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-06-29 23:27:15 -0700

    Add files via upload

---
+++ Functions in C++/Print all the elements.cpp
@@ -0,0 +1,32 @@
+/**
+ * Practice Problem ~ Shadman S.
+ * Topic: Functions 
+ * Problem Statement: given two numbers, write a function that prints all the elements present in the Array seperated by comma
+ */
+#include <iostream>
+using namespace std;
+
+
+void printArray(int a[], int n){
+    for (int i = 0; i < n ; i++){
+        if (a[i] == n ){
+            cout << a[i];
+        }
+        else{
+            cout << a[i] << ",";
+        }
+    }
+}
+
+int main(){
+    int a[] = {1,2,3,4,5,6,7,8,9,10};
+    
+    int n = sizeof(a)/sizeof(a[0]);
+    
+    cout << "The size of Array is " << n << " and the element that is present within the array is {";
+    
+    printArray (a,n);
+    
+    cout << "}";
+    
+}
