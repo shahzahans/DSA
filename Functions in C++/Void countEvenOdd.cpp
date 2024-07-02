84611a9e14ef68b05a3cfa1d2b443cad6bda086e
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-01 23:57:45 -0700

    Update Void countEvenOdd.cpp

--- Functions in C++/Void countEvenOdd.cpp
+++ Functions in C++/Void countEvenOdd.cpp
@@ -6,19 +6,18 @@
 #include <iostream>
 using namespace std;
 
-void countEvenOdd(int a[], int n){
-    int evenCount = 0;
-    int oddCount = 0;
-    for (int i = 0; i < n; i++){
-        if (a[i]%2==0){
+void countEvenOdd(int a[], int n){ // Function for counting even and Odd numbers
+    int evenCount = 0; // initialize the even count variable
+    int oddCount = 0; // initialize the odd count variable
+    for (int i = 0; i < n; i++){ // loop to search for even and odd numbers
+        if (a[i]%2==0){  // if statement to check for even number
             evenCount = evenCount + 1;
-    
         }
-        else{
+        else{ // else statment to check for odd number
             oddCount = oddCount + 1;
         }
     }
-    cout << "The count of even number is " << evenCount << " and the count number of odd is " << oddCount << endl;
+    cout << "The count of even number is " << evenCount << " and the count number of odd is " << oddCount << endl; // prints the odd and even number
 }
 
 int main()
