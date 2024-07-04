ff1c4e3b1f18dd562d744c11a0fc6f0358877ff8
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-04 11:12:24 -0700

    Update countPrime.cpp

--- Functions in C++/countPrime.cpp
+++ Functions in C++/countPrime.cpp
@@ -6,18 +6,18 @@
 #include <iostream>
 using namespace std;
 
-int checkPrime(int num){
+int checkPrime(int num){ // Function to check for prime numbers
     if (num <= 1) return 0; // "1" and "0" is not prime numbers
-    for(int i = 2; i < num; i++){
-        if (num%i == 0){
+    for(int i = 2; i < num; i++){ // loop to find the prime numbers within the array
+        if (num%i == 0){ // If number is not prime then its divisible return 0 or else returns 1
             return 0;
         }
     }
     return 1;
 }
-void countPrime(int a[], int n){
+void countPrime(int a[], int n){ // Function to count the prime numbers within the array
     int primCount = 0;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){ // loop to count 
         if(int flag = checkPrime(a[i])){
             primCount += flag;
         }
@@ -27,7 +27,8 @@ void countPrime(int a[], int n){
 
 int main()
 {
-    int size;// if array is given int n = sizeof(a)/sizeof(a[0]); 
+    // initialize the size 
+    int size; // if array is given int n = sizeof(a)/sizeof(a[0]); 
     cout << "Enter the size of the array: ";
     cin >> size; 
     cout << endl;
