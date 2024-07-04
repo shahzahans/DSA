1192f33aafff299136c2a695487cb099b793e1d4
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-03 17:28:37 -0700

    Update sumDigits.cpp

--- Functions in C++/sumDigits.cpp
+++ Functions in C++/sumDigits.cpp
@@ -1,27 +1,27 @@
 /**
  * Practice Problem ~ Shadman S.
  * Topic: Functions - sum of the digits of a given number
- * Problem Statement: given a numbers as an input, write a function that return the sum of all the digits of this number
+ * Problem Statement: given numbers as input, write a function that returns the sum of all the digits of this number
  */
 #include <iostream>
 using namespace std;
 
-int sumInt(int n){
-    int sum = 0;
-    while (n!= 0){
-        int digit = n%10;
-        sum = sum + digit;
-        n = n/10;
+int sumInt(int n){ // Function to add all the digits of this number
+    int sum = 0; // initializing a variable to store the sum of digits
+    while (n!= 0){ // while loop for summing all the digits of this given number
+        int digit = n%10; // first iteration 548/10 = remainder is 8, second iteration 54/10 = remainder is 4, third iteration 5/10 = remainder is  5
+        sum = sum + digit; 
+        n = n/10; // first iteration 548/10 = 54, second iteration 54/10 = 5
     }
-    return sum;
+    return sum; // return sum of all digits of the given number
 }
 
 int main(){
-    int number;
+    int number; // intialzie number variable
 
     cout << "Enter a number: " << endl;
-    cin >> number;
-    int n = number;
+    cin >> number; // takes in a number as an input
+    int n = number; // another initializing for calling the input number for later use if needed
     
-    cout << "The sum of all the digits of the number " << n << " is " << sumInt(n);
+    cout << "The sum of all the digits of the number " << n << " is " << sumInt(n); // finally resutls
 }
