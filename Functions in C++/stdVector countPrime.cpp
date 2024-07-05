49c7af8876f748ad615672233c0c7a90745d6e35
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-05 00:48:59 -0700

    Update stdVector countPrime.cpp

--- Functions in C++/stdVector countPrime.cpp
+++ Functions in C++/stdVector countPrime.cpp
@@ -7,20 +7,20 @@
 #include <vector>
 using namespace std;
 
-int checkPrime(int num){
+int checkPrime(int num){ // Function to check prime numbers 
     if (num <= 1) return 0; // "1" and "0" is not a prime numbers
-    for(int i = 2; i < num; i++){
-        if (num%i == 0){
+    for(int i = 2; i < num; i++){ // loop to see if a number is divisible or not
+        if (num%i == 0){ // if divisible return 0
             return 0;
         }
     }
-    return 1;
+    return 1; // if not divisible return 1
 }
-void countPrime(const vector<int>& a){
-    int primCount = 0;
-    for (int num : a){
-        if(checkPrime(num)){
-            primCount ++;
+void countPrime(const vector<int>& a){ // Function to count prime
+    int primCount = 0; // Initialize varible to count for prime numbers
+    for (int num : a){ // loop where nums takes each numbers in a
+        if(checkPrime(num)){ // calling funtion 
+            primCount ++; // return value adds up
         }
     }
     cout << "The number of prime element present in array is " << primCount << endl;
