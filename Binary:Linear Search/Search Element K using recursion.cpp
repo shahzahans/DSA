f66f65c9fddea034fcbebe20cc1a9bd4f8325c72
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-07 23:38:33 -0700

    Add files via upload

---
+++ Binary:Linear Search/Search Element K using recursion.cpp
@@ -0,0 +1,50 @@
+/**
+ * Practice Problem ~ Shadman S.
+ * Topic: "Time Comp" ~ Binary Search O(log(n))/Linear Search O(n)
+ * ~ Search Element K in sorted Array
+ * Problem Statement: Given a sorted array of integers and an element K, write a function which returns the index on 
+ * ~ which k is present, using loops
+ */
+#include <iostream>
+using namespace std;
+
+int linearSearch(int a[], int i, int n, int k){
+    if(i == n){
+        return -1;
+    }
+    if ( a[i] == k){
+        return i;
+    }
+    return linearSearch (a, i+1, n, k);
+}
+
+int binarySearch(int a[], int S, int E, int k){
+    int mid = (S+E)/2;
+    if(a[mid] == k){
+        return mid;
+    }
+    else if (a[mid] > k){
+        return binarySearch(a,S,E-1,k);
+        
+    }
+    else{
+        return binarySearch(a,S+1,E,k);
+    }
+    return -1;
+}
+
+
+int main(){
+ int a[] = {1,3,6,8,9,11};
+ int k = 6;
+ int n = sizeof(a)/sizeof(a[0]);
+ int S = 0;
+ int E = n-1;
+ int index1 = linearSearch(a,0,n,k);
+ int index2 = binarySearch(a,S,E,k);
+ 
+ cout << "The element " << k << " using linearSearch was found at index: " << index1 << endl;
+ cout << "The element " << k << " using binarySearch was found at index: " << index2 << endl;
+
+    return 0;
+}
