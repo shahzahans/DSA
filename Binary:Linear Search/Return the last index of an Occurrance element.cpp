732fc6075c4d7a739cda56e7f0a00a491150feab
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-11 23:32:57 -0700

    Update Return the last index of an Occurrance element.cpp

--- Binary:Linear Search/Return the last index of an Occurrance element.cpp
+++ Binary:Linear Search/Return the last index of an Occurrance element.cpp
@@ -2,13 +2,22 @@
  * Practice Problem ~ Shadman S.
  * Topic: "Time Comp" ~ Binary Search
  * ~ Search Element K in sorted Array
- * Problem Statement: Given a sorted array of integers and an element K, write a function which returns the last index of 
+ * Problem Statement: Given a sorted array of integers and an element K, write a function that returns the last index of 
  * ~ of an element k is present, using recursion 
- * - occurence element of last index is found through BinarySearch only.
+ * - The occurrence element of the last index is found through BinarySearch only.
  * return -1 if the element is not present
  */
 #include <iostream>
 using namespace std;
+
+int linearSearch(int a[], int n, int k){
+    for (int i = n -1; i >= 0; i--){
+        if(a[i] == k){
+            return i;
+        }
+    }
+    return -1;
+}
 
 int BinarySearch(int a[], int L, int R, int k){
     int res = -1;
@@ -35,8 +44,9 @@ int main()
     int n = sizeof(a)/ sizeof(a[0]);
 
     int LastIndex = BinarySearch(a, 0, n-1, k);
+    int LastIndex1 = linearSearch(a, n , k);
     
     cout <<"Last time " << k << " appears is in index: a[" << LastIndex << "]" << endl;
-    
+    cout <<"Last time " << k << " appears is in index: a[" << LastIndex1 << "]" << endl;
     return 0;
 }
