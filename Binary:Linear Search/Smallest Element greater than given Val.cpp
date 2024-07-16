56b6cef3043cd535d12e9bc097d026a64cf86f92
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-16 00:45:54 -0700

    Update Smallest Element greater than given Val.cpp

--- Binary:Linear Search/Smallest Element greater than given Val.cpp
+++ Binary:Linear Search/Smallest Element greater than given Val.cpp
@@ -8,21 +8,21 @@
 #include <iostream>
 using namespace std;
 
-char solve_bs (int a[], int n, int k){
-    int l = 0;
-    int r = n-1;
+char solve_bs (int a[], int n, int k){ // function to get the first index
+    int l = 0; // left side pointer
+    int r = n-1;// end of index pointer
     
-    int res = -1;
-    while(l <= r){
-        int mid = (l+r)/2;
-        if (a[mid] == k){
-            l = mid +1;
+    int res = -1; // initialized the res variable store the mid index through out the loop
+    while(l <= r){ // loop to find the element thet is bigger than k but lower than rest of the element
+        int mid = (l+r)/2; // to find the mid index
+        if (a[mid] == k){ // if mid is equal to k than left side points to mid +1
+            l = mid +1; 
         }
-        else if(a[mid] > k){
+        else if(a[mid] > k){ // if mid is bigger than k then, store the mid in res variable and r pointer moves -1
             res = a[mid];
             r = mid -1;
         }
-        else{
+        else{ // else left side point to mid +1
             l = mid +1;
         }
     }
@@ -31,11 +31,11 @@ char solve_bs (int a[], int n, int k){
 
 int main()
 {
-    int a[] = {1,3,5,6,7,9};
-    int k = 9;
-    int n = sizeof(a)/sizeof(a[0]);
+    int a[] = {1,3,5,6,7,9}; // initialied the array of integers
+    int k = 9; // k is the element to find
+    int n = sizeof(a)/sizeof(a[0]); // size of the array
     
-    int val = solve_bs(a,n,k);
+    int val = solve_bs(a,n,k); // calling the function
     if (val == -1){
         cout << "None of the Element are greater than " << k << endl;
     }
