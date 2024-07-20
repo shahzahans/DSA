7da1f83564ed1a75a8911234e5c19ac49df8c8f8
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-20 00:28:28 -0700

    Update Printing array using Recursion.cpp

--- Recursion/Printing array using Recursion.cpp
+++ Recursion/Printing array using Recursion.cpp
@@ -7,26 +7,26 @@
 #include <iostream>
 using namespace std;
 
-void recursion(int a[], int n, int i){
-    if (i == n){
+void recursion(int a[], int n, int i){ // recursion function to print all the arrays
+    if (i == n){ //if i equal to n the n then functions exits
         return;
     }
-    else if (i == n-1){
+    else if (i == n-1){ // else-if case to print the last array without the ","
         cout << a[i];
     }
     else{
-        cout << a[i] << ",";
+        cout << a[i] << ","; //else print all other array elements
     }
-    recursion(a,n,i+1);
+    recursion(a,n,i+1); //calling the recusrion function
 }
 
 int main()
 {
-    int a[] = {1,5,6,7,8,9,10};
-    int n = sizeof(a)/sizeof(a[0]);
+    int a[] = {1,5,6,7,8,9,10}; // initialized the arrays
+    int n = sizeof(a)/sizeof(a[0]); // finding the size of array
     
     cout << "The elements in the array are {";
-    recursion(a,n,0);
+    recursion(a,n,0); // calling the function
     cout << "}";
 
     return 0;
