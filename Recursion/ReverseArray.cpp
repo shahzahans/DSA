383b50495d7aec2c92f6660f9a6265a979054571
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-23 23:10:32 -0700

    Update ReverseArray.cpp

--- Recursion/ReverseArray.cpp
+++ Recursion/ReverseArray.cpp
@@ -14,19 +14,19 @@ void recursion(int a[], int n, int i){ //Function Print the array
     cout << a[i] << " "; //priting the array
     recursion(a,n,i+1); //callig the function itself
 }
-void reverseArray(int a[], int n, int i){ //
-    if(i < 0){
+void reverseArray(int a[], int n, int i){ //Function to print the array in reverse order
+    if(i < 0){ //if i is less than 0 then exits the function
         return;
     }
-    cout << a[i] << " ";
-    reverseArray(a,n,i -1);
+    cout << a[i] << " "; //Printing the array
+    reverseArray(a,n,i -1); // calling the function itself
 }
 int main()
 {
-    int a[] = {1,2,3,4,5,6,7,8,9,10};
-    // output {10,9,8,7,6,5,4,3,2,1}
-    int n = sizeof(a)/sizeof(a[0]);
-    int current = n -1;
+    int a[] = {1,2,3,4,5,6,7,8,9,10}; //intializing the array
+    // output {10,9,8,7,6,5,4,3,2,1} 
+    int n = sizeof(a)/sizeof(a[0]); //finding the size of the array
+    int current = n -1; //declaring variable for pointing to index
     
     cout << "The reverse Array of";
     recursion(a,n,0);
