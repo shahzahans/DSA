e97bc407a1c77203434e44589d302e6d5f726a92
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-24 23:07:31 -0700

    Update ReverseArray.cpp

--- Recursion/ReverseArray.cpp
+++ Recursion/ReverseArray.cpp
@@ -2,7 +2,7 @@
  * Practice Problem ~ Shadman S.
  * Topic: Recursion
  * ~ Reverse Array
- * Problem Statement: Given an array of integers, print all its element in reverse order using recursion
+ * Problem Statement: Given an array of integers, print all its elements in reverse order using recursion
  */
 #include <iostream>
 using namespace std;
