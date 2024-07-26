ddf48d64b7de6ed4b91168d3929127a455ea9a5f
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-26 00:09:45 -0700

    Update Fibonacci 2.cpp

--- Recursion/Fibonacci 2.cpp
+++ Recursion/Fibonacci 2.cpp
@@ -2,20 +2,20 @@
  * Practice Problem ~ Shadman S.
  * Topic: Recursion
  * ~ Fibonacci 
- * Problem Statement: given a integer n, find fibonacci series to that term
+ * Problem Statement: given an integer n, find Fibonacci series to that term
  */
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n){
+int fibonacci(int n){ //function to find the Fibonacci series
     //base case
-    if (n == 0){
+    if (n == 0){ //if n is 0 then returns 0
         return 0;
     }
-    else if (n == 1){
+    else if (n == 1){ //if n is 1 returns 1
         return 1;
     }
-    return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacci(n-1) + fibonacci(n-2); // for fibonacci to occur previous add two numbers needs to be added to get the next one
 }
 int main()
 {
