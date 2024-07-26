5ad6bb7afdb8c832e8e90b4736ce1294939a7fc6
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-25 23:27:20 -0700

    Add files via upload

---
+++ Recursion/Fibonacci 1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Practice Problem ~ Shadman S.
+ * Topic: Recursion
+ * ~ 
+ * Problem Statement: given a number n, find its factorial using recursion
+ */
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+    //base case
+    if (n == 0){
+        return 0;
+    }
+    else if (n == 1){
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+int main()
+{
+    int n;
+    //output 0,1,1,2,3,5
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << endl;
+    
+    cout << "The " << n << "'th term of the fibonacci series is "<< fibonacci (n) << endl;
+
+    return 0;
+}
