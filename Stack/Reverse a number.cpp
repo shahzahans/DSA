9fb2ec85e5e5a6c3586bb3d1ed34eea799490efe
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-27 22:10:06 -0700

    Update Reverse a number.cpp

--- Stack/Reverse a number.cpp
+++ Stack/Reverse a number.cpp
@@ -1,11 +1,8 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+/**
+ * Practice Problem ~ Shadman S.
+ * Topic: Stack
+ * Problem Statement: given numbers of ineteger, reverse it using a stack
+ */
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
