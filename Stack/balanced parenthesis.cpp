c9ed85ac96d7ca7fa18f30abb77bd92679a448b1
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-07-29 23:38:29 -0700

    Update balanced parenthesis.cpp

--- Stack/balanced parenthesis.cpp
+++ Stack/balanced parenthesis.cpp
@@ -15,17 +15,17 @@ int main()
     int n = a.size(); //finding the size of string
     stack <char> st; //declaring a stack method to store string as char
     
-    for(int i; i < n; i++){ 
-        if(st.size() != 0 && st.top() == '(' && a[i] == ')'){
+    for(int i; i < n; i++){ //loop to find if the parenthesis of string is balance or not
+        if(st.size() != 0 && st.top() == '(' && a[i] == ')'){ //if size of stack is not 0 and current top() begin parenthisis and a[i] is end parenthesis then pop()
             st.pop();
         }
-        else if(st.size() != 0 && st.top() == '[' && a[i] == ']'){
+        else if(st.size() != 0 && st.top() == '[' && a[i] == ']'){//if size of stack is not 0 and current top() begin parenthisis and a[i] is end parenthesis then pop()
             st.pop();
         }
-        else if (st.size() != 0 && st.top() == '{' && a[i] == '}'){
+        else if (st.size() != 0 && st.top() == '{' && a[i] == '}'){//if size of stack is not 0 and current top() begin parenthisis and a[i] is end parenthesis then pop()
             st.pop();
         }
-        else{
+        else{ //if stack is empty push the string as character
             st.push(a[i]);
         }
     }
