0fb181814048bcff728845fa4a5b493d7b089aea
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-08-09 23:47:54 -0700

    Update Printing the element of the queue.cpp

--- Queue/Printing the element of the queue.cpp
+++ Queue/Printing the element of the queue.cpp
@@ -11,14 +11,14 @@ int main()
 {
     int a[] = {1,2,3,4,5,6,7,8,9,10}; //initializing array of integers
     int n = sizeof(a)/sizeof(a[0]); //finding the size of Array
-    queue <int> queue1; //declaring queue
+    queue <int> queue1; //declaring queue1 using queue method
     
-    for(int i = 0; i<n; i++){
+    for(int i = 0; i<n; i++){ //for loop to push array of elements into queue1
         queue1.push(a[i]);
     }
     
     cout << "The Elements of the queue are: ";
-    while(!queue1.empty()){
+    while(!queue1.empty()){ //while queue1 is not empty print first element in the array
         cout << queue1.front() << " ";
         queue1.pop();
     }
