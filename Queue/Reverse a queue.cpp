d8ef50665e10479f34938f4b2dc08d62d2f85e28
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-08-11 01:30:21 -0700

    Update Reverse a queue.cpp

--- Queue/Reverse a queue.cpp
+++ Queue/Reverse a queue.cpp
@@ -9,16 +9,16 @@ using namespace std;
 
 int main()
 {
-    int a[] = {1,2,3,4,5,6,7,8,9,10};
-    int n = sizeof(a)/sizeof(a[0]);
+    int a[] = {1,2,3,4,5,6,7,8,9,10}; //intializing array
+    int n = sizeof(a)/sizeof(a[0]); //finding the size of an array
     
-    queue <int> queue1;
+    queue <int> queue1; //declaring queue
     
-    for (int i = n-1; i > -1; i--){
+    for (int i = n-1; i > -1; i--){ //loop to push array's element into the queue
         queue1.push(a[i]);
     }
     
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n; i++){ //loop to print the elements that is stored in queue
         cout << queue1.front() << " ";
         queue1.pop();
     }
